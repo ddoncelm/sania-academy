6044e34eab40baecc085f21c59a513d0e127921b
DoncelProject <doncel.project@gmail.com>
2026-05-07 19:44:35 +0200

    OTP login

---
+++ public/lessons/rename_lecciones.bat
@@ -0,0 +1,25 @@
+@echo off
+cd /d "%~dp0"
+echo Renombrando archivos...
+
+rename "leccion_2_10_simulaciones.html" "leccion_2_10_simulaciones_roleplaying.html"
+rename "leccion_2_1_bigdata_ML_DL_clinicos.html" "leccion_2_1_bigdata_ml_dl.html"
+rename "leccion_2_5_protocolo_etico.html" "leccion_2_5_prompts_eticos.html"
+rename "leccion_2_9_rag.html" "leccion_2_9_rag_validacion.html"
+rename "leccion_3_10_visualizacion.html" "leccion_3_10_visualizacion_datos.html"
+rename "leccion_3_14_simulacion.html" "leccion_3_14_simulacion_flujos.html"
+rename "leccion_3_1_casos_uso_automatizacion.html" "leccion_3_1_casos_automatizacion.html"
+rename "leccion_3_4_recordatorios.html" "leccion_3_4_recordatorios_derivaciones.html"
+rename "leccion_3_7_comunicaciones.html" "leccion_3_7_comunicaciones_clinicas.html"
+rename "leccion_3_8_hce_integracion.html" "leccion_3_8_simulaciones_HCE.html"
+rename "leccion_3_9_dashboards.html" "leccion_3_9_dashboards_apis.html"
+rename "leccion_4_10_datos_clinicos.html" "leccion_4_10_uso_legal_datos.html"
+rename "leccion_4_1_fundamentos_entrenamiento.html" "leccion_4_1_entrenamiento_salud.html"
+rename "leccion_5_11_roleplay_simulacion.html" "leccion_5_11_roleplaying_simulacion.html"
+rename "leccion_5_1_marco_normativo.html" "leccion_5_1_marco_normativo_esp.html"
+rename "leccion_5_7_informes_clinicos.html" "leccion_5_7_automatizacion_informes.html"
+rename "leccion_5_8_integracion_hce.html" "leccion_5_8_integracion_HCE.html"
+rename "leccion_5_9_roi_economico.html" "leccion_5_9_analisis_economico.html"
+
+echo Listo.
+pause
